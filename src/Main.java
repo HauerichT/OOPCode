e969e83e08c9b42ebebdd15237f9f2fe8ebc4551
timohaverich <timo.haverich@fh-bielefeld.de>
2022-12-15 20:50:08 +0100

    rotating

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[][] bild = {{1,0,0}, {0,1,0}, {0,0,1}};
+        int[][] bild = {{0,0,0,1}, {0,1,1,0}, {0,0,1,1}, {0,0,1,1}};
         OOPCode oopCode = new OOPCode(bild);
-        System.out.println(oopCode.berechneOOPCode());
+        oopCode.printBild();
+        oopCode.berechneOOPCode();
+
+        System.out.println();
+
+        oopCode.bildDrehen();
+        oopCode.printBild();
+        oopCode.berechneOOPCode();
+
+        System.out.println();
+
+        oopCode.bildDrehen();
+        oopCode.printBild();
+        oopCode.berechneOOPCode();
+
+        System.out.println();
+
+        oopCode.bildDrehen();
+        oopCode.printBild();
+        oopCode.berechneOOPCode();
 
     }
 }
--- src/OOPCode.java
+++ src/OOPCode.java
@@ -2,7 +2,14 @@ public class OOPCode {
 
     private int[][] bild;
     private int oopcode = 0;
+
     public OOPCode(int[][] bild) {
+
+        if (bild.length == 0) {
+            System.out.println("Leeres Bild übergeben!");
+            System.exit(0);
+        }
+
         for (int i = 0; i < bild.length; i++) {
             if (bild[i].length != bild.length) {
                 System.out.println("Kein NxN Format!");
@@ -18,8 +25,7 @@ public class OOPCode {
         this.bild = bild;
     }
 
-    public int berechneOOPCode() {
-
+    public void berechneOOPCode() {
         int fieldCounter = 1;
         for (int i = 0; i < bild.length; i++) {
             for (int j = 0; j < bild[i].length; j++) {
@@ -29,6 +35,44 @@ public class OOPCode {
                 fieldCounter++;
             }
         }
-        return oopcode;
+        System.out.println("OOPCode = " + this.oopcode);
+        oopcode = 0;
     }
+
+    public void printBild() {
+        for (int i = 0; i < bild.length; i++) {
+            for (int j = 0; j < bild[i].length; j++) {
+                if (j == (bild[i].length)-1) {
+                    System.out.print(bild[i][j]);
+                }
+                else {
+                    System.out.print(bild[i][j] + " | ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void bildDrehen() {
+        int n = bild.length;
+
+
+        for (int i = 0; i < n; i++){
+            for (int j = i; j < n; j++){
+                int temp = bild[i][j];
+                bild[i][j] = bild[j][i];
+                bild[j][i] = temp;
+            }
+        }
+        // Reihenfolge der Zeilen vertauschen
+        for (int i = 0; i < n; i++){
+            for(int j = 0; j < n/2; j++){
+                int temp = bild[i][j];
+                bild[i][j] = bild[i][n - 1 - j];
+                bild[i][n - 1 - j] = temp;
+            }
+        }
+    }
+
+
 }
